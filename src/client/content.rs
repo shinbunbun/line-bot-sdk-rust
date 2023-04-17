4207510cb448a3fe82c46c6b0870f8c8a6ea3d7e
shinbunbun <takamuneyuto@gmail.com>
2023-04-17 09:43:09 +0900

    add get_message_content_transcoding

--- src/client/content.rs
+++ src/client/content.rs
@@ -1,7 +1,14 @@
-use crate::awc_wrapper::SendClientRequestByteFut;
+use serde::{Deserialize, Serialize};
+
+use crate::awc_wrapper::{SendClientRequestByteFut, SendClientRequestFut};
 use crate::client::API_DATA_ENDPOINT_BASE;
 
 use crate::Client;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetMessageContentTranscodingResponse {
+    pub status: String,
+}
 
 impl Client {
     pub fn get_message_content(&self, message_id: &str) -> SendClientRequestByteFut {
@@ -10,5 +17,16 @@ impl Client {
             API_DATA_ENDPOINT_BASE, message_id
         );
         SendClientRequestByteFut::new(self.get(&url, None::<&[(); 0]>, None, true))
+    }
+
+    pub fn get_message_content_transcoding(
+        &self,
+        message_id: &str,
+    ) -> SendClientRequestFut<GetMessageContentTranscodingResponse> {
+        let url = format!(
+            "{}/v2/bot/message/{}/content/transcoding",
+            API_DATA_ENDPOINT_BASE, message_id
+        );
+        SendClientRequestFut::new(self.get(&url, None::<&[(); 0]>, None, true))
     }
 }
